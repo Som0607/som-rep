e3cbe4aa50bd7593ccb142056b5c93d2864ba895
Som0607 <56604296+Som0607@users.noreply.github.com>
2020-08-10 11:32:48 +0530

    complete

---
+++ start/starting js/types/js/script.js
@@ -0,0 +1,20 @@
+// var x;
+// console.log(x);
+
+// if (x == undefined) {
+// 	console.log("x is undefied");
+// }
+
+// x = 5;
+// if (x == undefined) {
+// 	console.log("x is undefied");
+// }
+// else {
+// 	console.log("x has been defined");
+// }
+
+
+var x = 10;
+if ( (null) || (console.log("Hello")) || x > 5 ) {
+  console.log("Hello");
+}
